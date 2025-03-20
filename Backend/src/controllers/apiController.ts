3be5f827b4a63465abd5d342d1df6fc7b7158836
KAMAL-02 <Kamalnayan403@gmail.com>
2025-03-20 22:08:26 +0530

    QueryParam and error handle

--- Backend/src/controllers/apiController.ts
+++ Backend/src/controllers/apiController.ts
@@ -1,26 +1,44 @@
-import axios from 'axios';
-import { Request, Response } from 'express';
+import axios from "axios";
+import { Request, Response } from "express";
 
-const sendApiRequest = async(req: Request, res: Response) => {
-    console.log("Request body is: ", req.body);
-    const { method, url } = req.body;
-    if (!url || !method) {
-        res.status(400).json({ message: 'All fields are required' });
-        return ;
-    }
-    try {
-        const response = await axios({method, url});
-        if(!response) {
-            res.status(404).json({ message: 'Not found' });
-            return ;
+const sendApiRequest = async (req: Request, res: Response) => {
+  console.log("Request body is: ", req.body);
+  const { method, url, params } = req.body;
+  if (!url || !method) {
+    res.status(400).json({ message: "All fields are required" });
+    return;
+  }
+  try {
+    const queryString = new URLSearchParams(params).toString();
+    const fullUrl = queryString ? `${url}?${queryString}` : url;
+
+    console.log("Full URL:", fullUrl);
+    const response = await axios({ method, url, params: params || {} });
+
+    res.status(response.status).json({ status: response.status, data: response.data });
+  } catch (error) {
+    console.error(error);
+
+    // Error from the url provided
+    if(axios.isAxiosError(error)){
+        if(error.response){
+            res.status(error.response.status).json({ status: error.response.status, data: error.response.data });
+            return;
         }
-        res.status(200).json({ status: response.status, data: response.data });
-        return;
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
-        return;
+
+        // Error from the request like timeout or network error
+        if(error.request){
+            res.status(502).json({
+                message: "Bad Gateway",
+            });
+            return;
+        }
     }
-}
+
+    // Internal server error
+    res.status(500).json({ message: "Internal server error" });
+    return;
+  }
+};
 
 export { sendApiRequest };
