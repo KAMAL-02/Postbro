b70c081593fdd2500c3ae1b45aaa359ee499c0c8
KAMAL-02 <Kamalnayan403@gmail.com>
2025-05-22 17:19:49 +0530

    Dep fix

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,9 +3,11 @@ import {inter, jetbrainsMono} from '../utils/fonts'
 import Rest from '@/components/rest/rest'
 
 const page = () => {
+  //test line
   return (
     <div className='h-full w-full'>
       <Rest />
+      
     </div>
   )
 }
